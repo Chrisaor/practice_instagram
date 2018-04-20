428ac048764acc838b23c4cfab6d7d0f5597d56e
chrisaor <pchpch0070@gmail.com>
2018-04-20 14:31:50 +0900

    base.html구현 및 login 유무에 따른 상단바 변화 적용

--- app/config/urls.py
+++ app/config/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from instagram import views as instagram_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', instagram_views.IndexView.as_view(), name = 'root'),
+    path('instagram/', include('instagram.urls')),
+
 ]
